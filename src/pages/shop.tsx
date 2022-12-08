1206e16b71c8ccde258a2bf66eb5caf708dbf38b
DeveloperFG <firatgoktepe1986@gmail.com>
2022-12-08 18:10:11 +0300

    shop page template created

---
+++ src/pages/shop.tsx
@@ -0,0 +1,51 @@
+import { getNextStaticProps } from "@faustjs/next";
+import { client } from "client";
+import { Footer, Header, Hero } from "components";
+import { GetStaticPropsContext } from "next";
+import Head from "next/head";
+
+export default function Shop() {
+  const { useQuery } = client;
+  const generalSettings = useQuery().generalSettings;
+
+  return (
+    <>
+      <Header
+        title={generalSettings.title}
+        description={generalSettings.description}
+      />
+
+      <Head>
+        <title>Ürünlerimiz - {generalSettings.title}</title>
+      </Head>
+
+      <Hero title="Ürünlerimiz" />
+
+      <main className="content content-single">
+        <div className="wrap">
+          <p>
+            You can still create pages just as you would in{" "}
+            <a
+              href="https://nextjs.org/docs/basic-features/pages"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Next.js
+            </a>
+            . Take a look at <code>src/pages/custom-page.tsx</code> for an
+            example.
+          </p>
+        </div>
+      </main>
+
+      <Footer copyrightHolder={generalSettings.title} />
+    </>
+  );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return getNextStaticProps(context, {
+    // Page,
+    client,
+  });
+}
