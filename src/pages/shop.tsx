23f7d2f4baaba9876eea6a20e51af4bfd31bf547
DeveloperFG <firatgoktepe1986@gmail.com>
2022-12-09 20:17:41 +0300

    shop page regulated

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -1,12 +1,26 @@
 import { getNextStaticProps } from "@faustjs/next";
 import { client } from "client";
-import { Footer, Header, Hero } from "components";
+import { Footer, Header, Hero, Posts } from "components";
 import { GetStaticPropsContext } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
+
+const POSTS_PER_PAGE = 6;
 
 export default function Shop() {
-  const { useQuery } = client;
+  const { useQuery, usePosts } = client;
+  const { query = {} } = useRouter();
   const generalSettings = useQuery().generalSettings;
+  const { postSlug, postCursor } = query;
+  const isBefore = postSlug === "before";
+  const posts = usePosts({
+    after: !isBefore ? (postCursor as string) : undefined,
+    before: isBefore ? (postCursor as string) : undefined,
+    first: !isBefore ? POSTS_PER_PAGE : undefined,
+    last: isBefore ? POSTS_PER_PAGE : undefined,
+  });
+
+  console.log("dsada", client.client);
 
   return (
     <>
@@ -22,20 +36,13 @@ export default function Shop() {
       <Hero title="Ürünlerimiz" />
 
       <main className="content content-single">
-        <div className="wrap">
-          <p>
-            You can still create pages just as you would in{" "}
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Next.js
-            </a>
-            . Take a look at <code>src/pages/custom-page.tsx</code> for an
-            example.
-          </p>
-        </div>
+        <Posts
+          posts={posts.nodes}
+          heading="Latest Posts"
+          intro="The Posts component in src/pages/index.tsx shows the latest six posts from the connected WordPress site."
+          headingLevel="h2"
+          postTitleLevel="h3"
+        />
       </main>
 
       <Footer copyrightHolder={generalSettings.title} />
