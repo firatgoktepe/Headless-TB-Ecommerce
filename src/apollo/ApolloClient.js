59afee63fc0579c8eb3f725982946223828814c2
DeveloperFG <firatgoktepe1986@gmail.com>
2022-12-21 17:37:56 +0300

    apollo file added

---
+++ src/apollo/ApolloClient.js
@@ -0,0 +1,30 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const defaultOptions = {
+    watchQuery: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore'
+    },
+    query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all'
+    }
+};
+
+const cache = new InMemoryCache({
+    resultCaching: false
+});
+
+const link = createHttpLink({
+    uri: `${process.env.NEXT_PUBLIC_WOO_SITE_URL}/graphql`
+});
+
+// Apollo GraphQL client.
+const client = new ApolloClient({
+    link,
+    cache,
+    defaultOptions,
+    connectToDevTools: true
+});
+
+export default client;
